afff0b2f8da4ad54112fe059508dbf13a4f242b9
wajahat494 <bankawajid630@gmail.com>
2025-10-15 21:24:57 +0530

    Update createtarverse.c

--- createtarverse.c
+++ createtarverse.c
@@ -4,30 +4,50 @@ struct Node{
     int data;
     struct Node* next;
 };
-void traversealinkedlist(struct Node* ptr){
-    while(ptr!=NULL){
-    printf("element:%d\n",ptr->data);
-    ptr=ptr->next;
+void traverse(struct Node* ptr){
+    while(ptr!=0){
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
     }
 }
+//insert at begining
+struct Node* begining(struct Node* head,int data){
+    struct Node* ptr=(struct Node*)malloc(sizeof(struct Node));
+    ptr->next=head;
+    ptr->data=data;
+    return ptr;
+}
+    
+    //insert in between
+    struct Node* between(struct Node* head,int data,int index){
+    struct Node* ptr=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* p=head;
+    int i=0;
+    while(i!=index-1){
+     
+    p=p->next;
+     i++;
+     }
+     ptr->data=data;
+     ptr->next=p->next;
+     p->next=ptr;
+     return head;
+
+    }
 int main(){
-    struct Node* head=(struct Node*) malloc(sizeof(struct Node));
-    struct Node* second=(struct Node*) malloc(sizeof(struct Node));
-    struct Node* third=(struct Node*) malloc(sizeof(struct Node));
-    struct Node* fourth=(struct Node*)malloc(sizeof(struct Node));
-    head->data=7;
-    head->next=second;
-    second->data=8;
-    second->next=third;
-    third->data=9;
-    third->next=fourth;
-    fourth->data=6;
-    fourth->next=NULL;
-    traversealinkedlist(head);
-
-
-
-
-
-    return 0;
+struct Node* head=(struct Node*)malloc(sizeof(struct Node));
+struct Node* second=(struct Node*)malloc(sizeof(struct Node));
+struct Node* third=(struct Node*)malloc(sizeof(struct Node));
+head->data=7;
+head->next=second;
+second->data=5;
+second->next=third;
+third->data=8;
+third->next=NULL;
+traverse(head);
+head=begining(head,78);
+printf("\n");
+traverse(head);
+head=between(head,4,2);
+traverse(head);
 }
